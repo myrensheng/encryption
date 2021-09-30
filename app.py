535c1e382b44958fd6b7797fa795111a43f3734c
半生月 <18309169600@163.com>
2021-09-30 17:03:57 +0800

    开发进度70%

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 import json
 from flask_cors import *
-from tools import allowed_file, excel_path, get_table_values
+from tools import allowed_file, excel_path, get_table_values, md5_excel_path, encryption_clos
 import os
 
 app = Flask(__name__)
@@ -18,8 +18,12 @@ def upload():
     # 上传文件到 static/excel 文件夹中
     # 清空数据文件
     if os.listdir(excel_path):
-        exist_file = os.path.join(excel_path, os.listdir(excel_path)[0])
-        os.remove(exist_file)
+        excel_file = os.path.join(excel_path, os.listdir(excel_path)[0])
+        os.remove(excel_file)
+    # 清空已加密的excel文件
+    if os.listdir(md5_excel_path):
+        md5_excel_file = os.path.join(md5_excel_path, os.listdir(md5_excel_path)[0])
+        os.remove(md5_excel_file)
     if 'file' not in request.files:
         print("request中没有files参数")
         return jsonify({"status": 400, "msg": "请上传excel文件！"})
@@ -29,8 +33,6 @@ def upload():
         # 文件名不为空并且是excel文件
         filename = file.filename
         file.save(os.path.join(excel_path, filename))
-        # print(filename,file.filename)
-        # print("保存成功")
         return jsonify({"status": 200, "msg": filename + "保存成功！"})
     return jsonify({"status": 400, "msg": "请上传excel文件"})
 
@@ -46,16 +48,15 @@ def table():
 
 @app.route("/encryption", methods=["GET", "POST"])
 def encryption():
-
     r_json = request.json
-    print(r_json)
     encry_cols = r_json["encryCols"]
     encry_style = r_json["encry_style"]
     if False in [i.isnumeric() for i in encry_cols.split(",")]:
         return jsonify({"status": 400, "msg": "加密列以英文‘,’隔开！"})
     # 加密成功，直接返回加密后的数据
-
-    return jsonify({"status": 200, "msg": "加密成功！"})
+    encry_cols = [int(i) for i in encry_cols.split(",")]
+    md5_excel_values = encryption_clos(encry_cols, encry_style)
+    return jsonify({"status": 200, "msg": "加密成功！", "excel_values": md5_excel_values})
 
 
 if __name__ == '__main__':
--- tools.py
+++ tools.py
@@ -1,9 +1,11 @@
 import hashlib
 
 import xlrd
+import xlwt
 import os
 
 excel_path = os.path.join(os.getcwd(), "static", "excel")
+md5_excel_path = os.path.join(os.getcwd(), "static", "md5_excel")
 ALLOWED_EXTENSIONS = ['xlsx', 'xls']
 
 
@@ -12,7 +14,7 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-def md5_encryption(string,style="md5（小写）"):
+def md5_encryption(string, style="md5（小写）"):
     # m = hashlib.sha256()
     m = hashlib.md5()
     m.update(str(string).encode("utf8"))
@@ -23,17 +25,50 @@ def md5_encryption(string,style="md5（小写）"):
     return press
 
 
-def encryption_clos(encry_cols=None):
-    # 加密对应的列
+def encryption_clos(encry_cols=None, encry_style=None):
     # 当加密的是空字符串是不加密，保留空字符串
-
-    pass
-
-
-def get_table_values():
-    # 获取上传excel的前 5 行数据，默认加密第一个表中的数据
+    if not encry_style or not encry_cols:
+        return None
     if os.listdir(excel_path):
         filename = os.path.join(excel_path, os.listdir(excel_path)[0])
+        workbook = xlrd.open_workbook(filename=filename)
+        table = workbook.sheets()[0]
+        # excel 中没有数据
+        if table.ncols == 0:
+            return None
+        if max(encry_cols) - 1 > table.ncols:
+            # 输入的加密列不在excel中
+            return None
+        # 开始加密数据
+        md5_workbook = xlwt.Workbook()
+        work_sheet = md5_workbook.add_sheet("md5加密数据")
+        c = 0
+        for col in encry_cols:
+            r = 0
+            col_values = table.col_values(colx=col-1)
+            work_sheet.write(r, c, str(col))
+            work_sheet.write(r, c + 1, str(col) + "_"+encry_style)
+            for v in col_values:
+                if v == '':
+                    md5_v = v
+                else:
+                    md5_v = md5_encryption(v, style=encry_style)
+                work_sheet.write(r + 1, c, v)
+                work_sheet.write(r + 1, c + 1, md5_v)
+                r += 1
+            c += 2
+        md5_file = os.path.join(md5_excel_path,'md5加密数据.xlsx')
+        md5_workbook.save(md5_file)
+        # 返回md5文件的前5行数据
+        md5_table_info = get_table_values(file_path=md5_excel_path)
+        return md5_table_info
+    return None
+
+
+def get_table_values(file_path=excel_path):
+    # 获取上传excel的前 5 行数据，默认加密第一个表中的数据
+    if os.listdir(file_path):
+        filename = os.path.join(file_path, os.listdir(file_path)[0])
         workbook = xlrd.open_workbook(filename=filename)
         table = workbook.sheets()[0]
         # 默认返回前5行数据
@@ -50,4 +85,3 @@ def get_table_values():
         return {"table_cols": table.ncols, "row_5_list": row_5_list}
     # 没有excel文件或者上传的不是excel文件
     return None
-
