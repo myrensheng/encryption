bc79cbbbaa033a5108f7458287030eba8e925338
myrensheng <18309169600@163.com>
2021-10-31 22:43:58 +0800

    20211031

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, send_from_directory
 import json
 from flask_cors import *
-from tools import allowed_file, excel_path, get_table_values, md5_excel_path, encryption_clos
+from tools import allowed_file, excel_path, get_table_values, encry_excel_path, encryption_clos, encryption_str
 import os
 
 app = Flask(__name__)
@@ -21,9 +21,9 @@ def upload():
         excel_file = os.path.join(excel_path, os.listdir(excel_path)[0])
         os.remove(excel_file)
     # 清空已加密的excel文件
-    if os.listdir(md5_excel_path):
-        md5_excel_file = os.path.join(md5_excel_path, os.listdir(md5_excel_path)[0])
-        os.remove(md5_excel_file)
+    if os.listdir(encry_excel_path):
+        encry_excel_file = os.path.join(encry_excel_path, os.listdir(encry_excel_path)[0])
+        os.remove(encry_excel_file)
     if 'file' not in request.files:
         print("request中没有files参数")
         return jsonify({"status": 400, "msg": "请上传excel文件！"})
@@ -40,6 +40,7 @@ def upload():
 @app.route("/table", methods=["GET", "POST"])
 def table():
     excel_values = get_table_values()
+    # print(excel_values)
     if excel_values:
         return jsonify({"status": 200, "msg": "获取数据成功！", "excel_values": excel_values})
     else:
@@ -49,14 +50,47 @@ def table():
 @app.route("/encryption", methods=["GET", "POST"])
 def encryption():
     r_json = request.json
+    encry_content = r_json["encryContent"]
+    # 加密算法
+    encry_model = r_json["encryModel"]
+    # 加密类型（大写、小写）
+    encry_style = r_json["encryStyle"]
+    print(encry_content)
+    if encry_content == "textarea":
+        encry_textarea = str(r_json["input_textarea"])
+        res = {
+            "status": 200,
+            "output_textarea": encryption_str(encry_textarea, encry_model=encry_model, encry_style=encry_style),
+            "msg": "加密成功！"
+        }
+        return jsonify(res)
+
+    # 需要加密的列
     encry_cols = r_json["encryCols"]
-    encry_style = r_json["encry_style"]
-    if False in [i.isnumeric() for i in encry_cols.split(",")]:
+    if False in [i.isnumeric() for i in encry_cols.strip().strip(",").split(",")]:
         return jsonify({"status": 400, "msg": "加密列以英文‘,’隔开！"})
     # 加密成功，直接返回加密后的数据
-    encry_cols = [int(i) for i in encry_cols.split(",")]
-    md5_excel_values = encryption_clos(encry_cols, encry_style)
-    return jsonify({"status": 200, "msg": "加密成功！", "excel_values": md5_excel_values})
+    encry_cols = list(set([int(i) for i in encry_cols.split(",")]))
+    encry_excel_values = encryption_clos(encry_cols, encry_model, encry_style)
+    if isinstance(encry_excel_values, dict):
+        return jsonify({"status": 200, "msg": "加密成功！", "excel_values": encry_excel_values})
+    else:
+        return jsonify({"status": 400, "msg": encry_excel_values})
+
+
+@app.route("/download", methods=["GET", "POST"])
+def download():
+    # encry_file = os.path.join(encry_excel_path, 'encry加密数据.xlsx')
+    return send_from_directory(encry_excel_path, '加密数据.xlsx')
+
+
+# @app.route("/images",methods=["GET","POST"])
+# def images():
+#     if request.method == "GET":
+#         pics = request.args.to_dict()
+#         if pics.get("pic")==1:
+#             return ""
+#         return pics
 
 
 if __name__ == '__main__':
--- tools.py
+++ tools.py
@@ -5,7 +5,7 @@ import xlwt
 import os
 
 excel_path = os.path.join(os.getcwd(), "static", "excel")
-md5_excel_path = os.path.join(os.getcwd(), "static", "md5_excel")
+encry_excel_path = os.path.join(os.getcwd(), "static", "md5_excel")
 ALLOWED_EXTENSIONS = ['xlsx', 'xls']
 
 
@@ -14,20 +14,28 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-def md5_encryption(string, style="md5（小写）"):
-    # m = hashlib.sha256()
-    m = hashlib.md5()
-    m.update(str(string).encode("utf8"))
-    if style == 'md5（小写）':
-        press = m.hexdigest()
-    else:
-        press = m.hexdigest().upper()
-    return press
+def encryption_str(string, encry_model="md5_32", encry_style=True):
+    # 加密为 utf-8 编码
+    utf_8_str = str(string).encode("utf8")
+    # 函数字典
+    param_dict = {
+        "md5_32": hashlib.md5(utf_8_str),
+        "md5_16": hashlib.md5(utf_8_str),
+        "sha1": hashlib.sha1(utf_8_str),
+        "sha224": hashlib.sha224(utf_8_str),
+        "sha256": hashlib.sha256(utf_8_str),
+        "sha512": hashlib.sha512(utf_8_str)
+    }
+    encry_result = param_dict[encry_model].hexdigest()
+    if encry_model == 'md5_16':
+        encry_result = encry_result[8:-8]
+    # 返回结果
+    return encry_result if encry_style == "小写" else encry_result.upper()
 
 
-def encryption_clos(encry_cols=None, encry_style=None):
+def encryption_clos(encry_cols=None, encry_model=None, encry_style=None):
     # 当加密的是空字符串是不加密，保留空字符串
-    if not encry_style or not encry_cols:
+    if not encry_model or not encry_cols or not encry_style:
         return None
     if os.listdir(excel_path):
         filename = os.path.join(excel_path, os.listdir(excel_path)[0])
@@ -35,34 +43,34 @@ def encryption_clos(encry_cols=None, encry_style=None):
         table = workbook.sheets()[0]
         # excel 中没有数据
         if table.ncols == 0:
-            return None
+            return "excel 中没有数据！"
         if max(encry_cols) - 1 > table.ncols:
             # 输入的加密列不在excel中
-            return None
+            return str(max(encry_cols)) + "超过excel中最大的列"
         # 开始加密数据
-        md5_workbook = xlwt.Workbook()
-        work_sheet = md5_workbook.add_sheet("md5加密数据")
+        encry_workbook = xlwt.Workbook()
+        work_sheet = encry_workbook.add_sheet("md5加密数据")
         c = 0
         for col in encry_cols:
             r = 0
-            col_values = table.col_values(colx=col-1)
+            col_values = table.col_values(colx=col - 1)
             work_sheet.write(r, c, str(col))
-            work_sheet.write(r, c + 1, str(col) + "_"+encry_style)
+            work_sheet.write(r, c + 1, str(col) + "_" + encry_model + "_" + encry_style)
             for v in col_values:
                 if v == '':
-                    md5_v = v
+                    encry_v = v
                 else:
-                    md5_v = md5_encryption(v, style=encry_style)
+                    encry_v = encryption_str(string=v, encry_model=encry_model, encry_style=encry_style)
                 work_sheet.write(r + 1, c, v)
-                work_sheet.write(r + 1, c + 1, md5_v)
+                work_sheet.write(r + 1, c + 1, encry_v)
                 r += 1
             c += 2
-        md5_file = os.path.join(md5_excel_path,'md5加密数据.xlsx')
-        md5_workbook.save(md5_file)
+        encry_file = os.path.join(encry_excel_path, '加密数据.xlsx')
+        encry_workbook.save(encry_file)
         # 返回md5文件的前5行数据
-        md5_table_info = get_table_values(file_path=md5_excel_path)
-        return md5_table_info
-    return None
+        encry_table_info = get_table_values(file_path=encry_excel_path)
+        return encry_table_info
+    return "服務器內部錯誤"
 
 
 def get_table_values(file_path=excel_path):
@@ -72,16 +80,18 @@ def get_table_values(file_path=excel_path):
         workbook = xlrd.open_workbook(filename=filename)
         table = workbook.sheets()[0]
         # 默认返回前5行数据
-        table_rows = 5 if table.nrows >= 5 else table.nrows
+        # table_rows = 5 if table.nrows >= 5 else table.nrows
+        table_rows = table.nrows
         if table_rows == 0:
-            return None
-        # 获取前5行数据
-        row_5_list = []
+            return {"status": 400, "msg": "excel中没有数据！"}
+        # 获取数据
+        row_list = []
         for r in range(table_rows):
             row_dict = {}
             for k, v in enumerate(table.row_values(rowx=r)):
                 row_dict[str(k + 1)] = v
-            row_5_list.append(row_dict)
-        return {"table_cols": table.ncols, "row_5_list": row_5_list}
+            row_list.append(row_dict)
+        return {"table_cols": table.ncols, "row_list": row_list, "excel_name": os.listdir(file_path)[0],
+                "sheet_name": workbook.sheet_names()[0]}
     # 没有excel文件或者上传的不是excel文件
     return None
